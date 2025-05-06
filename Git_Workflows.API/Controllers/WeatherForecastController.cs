6769f820fa11db8f9d99831e9536295a965fb54d
Chirag Patel <chirag.p@shaligraminfotech.com>
2025-05-06 12:58:01 +0530

    Commit 15

--- Git_Workflows.API/Controllers/WeatherForecastController.cs
+++ Git_Workflows.API/Controllers/WeatherForecastController.cs
@@ -33,21 +33,5 @@ namespace Git_Workflows.API.Controllers
             })
             .ToArray();
         }
-
-        /// <summary>
-        /// Get method for weatherforcast
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("list")]
-        public IEnumerable<WeatherForecast> Get2(string Id)
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
