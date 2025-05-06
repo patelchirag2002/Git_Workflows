d855001fa31c14a90aaba9ed23b0b26caddc12a4
Chirag Patel <chirag.p@shaligraminfotech.com>
2025-05-06 11:09:13 +0530

    Added api routing check

--- Git_Workflows.API/Controllers/WeatherForecastController.cs
+++ Git_Workflows.API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Git_Workflows.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/WeatherForecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
