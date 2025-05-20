31a2d4e005bd1bb272ad8a2fb9cd6dd63a478d5e
Chirag Patel <chirag.p@shaligraminfotech.com>
2025-05-20 11:30:17 +0530

    Bug Fix

--- Git_Workflows.API/Controllers/WeatherForecastController.cs
+++ Git_Workflows.API/Controllers/WeatherForecastController.cs
@@ -25,11 +25,13 @@ namespace Git_Workflows.API.Controllers
         [HttpGet("list")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var Description = "Test";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Description = Description
             })
             .ToArray();
         }
